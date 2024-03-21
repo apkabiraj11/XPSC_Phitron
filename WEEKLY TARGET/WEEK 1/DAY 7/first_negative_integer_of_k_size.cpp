29f614620b758fb237d55770b375b10ba548c67a
apkabiraj11 <apkabiraj11@gmail.com>
2024-03-21 22:23:34 +0600

    week 1 day 7 code added

---
+++ WEEKLY TARGET/WEEK 1/DAY 7/first_negative_integer_of_k_size.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+              
+              int l=0,r=0;
+              queue<int>q;
+              vector<long long >ans;
+              while(r<N)
+              {
+                  if(A[r]<0)q.push(A[r]);
+                  if(r-l+1==K)
+                  {
+                    if(!q.empty())
+                    {
+                       ans.push_back(q.front());
+                       if(A[l]<0)q.pop();
+                       l++;
+                       r++;
+                    }
+                    else {
+                        ans.push_back(0);
+                        l++;
+                        r++;
+                  }
+                  }
+                  else 
+                   r++;
+                  
+              }
+              return ans;
+                                                 
+ }
